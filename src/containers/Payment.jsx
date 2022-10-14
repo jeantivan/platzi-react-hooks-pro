04ff1aedf3f3aedf48d54c48b3c25fd90c3a814b
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-14 18:36:13 -0400

    Maquetación y estilos del flujo de pago

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,7 +1,16 @@
+import '../styles/components/Payment.css';
+
 import React from 'react';
 
 const Payment = () => {
-  return <div>Payment</div>;
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido</h3>
+        <div className="Payment-button">boton con paypal</div>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,7 +1,17 @@
+import '../styles/components/Success.css';
+
 import React from 'react';
 
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Oscar, Gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 dias a tu dirección</span>
+        <div className="Success-map">Google maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
