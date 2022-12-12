da890c985c4d98b52c614933952365a3ec698a11
Jean Paul Tivan <jptivan53@gmail.com>
2022-12-12 19:53:23 -0400

    Integración de pagos con Paypal

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,69 @@
 import '../styles/components/Payment.css';
 
-import React from 'react';
+import React, { useContext } from 'react';
+import { PayPalButtons } from '@paypal/react-paypal-js';
+import { useNavigate } from 'react-router-dom';
 
+import AppContext from '../context/AppContext';
 const Payment = () => {
+  const navigate = useNavigate();
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+
+  const handleSumTotal = () => {
+    const reducer = (acc, curr) => acc + curr.product.price * curr.qty;
+
+    const sumTotal = cart.reduce(reducer, 0);
+
+    return sumTotal;
+  };
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+
+    const order = {
+      buyer,
+      products: cart,
+      payment: data,
+    };
+    addNewOrder(order);
+    navigate('/checkout/success');
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido</h3>
-        <div className="Payment-button">boton con paypal</div>
+        {cart.map(({ product, qty }) => (
+          <div className="Payment-item" key={product.title}>
+            <div className="Payment-element">
+              <h4>{product.title}</h4>
+              <span>
+                {qty} x ${product.price}
+              </span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-item" style={{ margin: '32px 0', fontSize: '1.25rem' }}>
+          <div className="Payment-element">
+            <h4>Total</h4>
+
+            <span style={{ fontWeight: 'bold' }}>${handleSumTotal()}</span>
+          </div>
+        </div>
+        <div className="Payment-button">
+          <PayPalButtons
+            style={{ layout: 'vertical' }}
+            amount={handleSumTotal()}
+            onClick={() => console.log('Payment start')}
+            onError={(error) => console.log(error)}
+            onCancel={(data) => {
+              console.log('Payment cancel by user');
+              console.log(data);
+            }}
+            onApprove={(data) => handlePaymentSuccess(data)}
+          />
+        </div>
       </div>
     </div>
   );
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,24 +8,35 @@ import Layout from '../components/Layout';
 import AppContext from '../context/AppContext';
 import useInitialState from '../hooks/useInitialState';
 
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+const paypalOptions = {
+  'client-id':
+    'AT3w_Zh5frhC_jPyTl7lj6C0VastCx4TB5r4uc4mHsob7VQdHTg-1Aue1puO8i-wE9IanYTQXTVK5Ref',
+  currency: 'USD',
+  intent: 'capture',
+};
+
 const App = () => {
   const initialState = useInitialState();
 
   return (
-    <AppContext.Provider value={initialState}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/checkout/information" element={<Information />} />
-            <Route path="/checkout/payment" element={<Payment />} />
-            <Route path="/checkout/success" element={<Success />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
+    <PayPalScriptProvider options={paypalOptions}>
+      <AppContext.Provider value={initialState}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/checkout/information" element={<Information />} />
+              <Route path="/checkout/payment" element={<Payment />} />
+              <Route path="/checkout/success" element={<Success />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppContext.Provider>
+    </PayPalScriptProvider>
   );
 };
 
