a097823cee0aa04ea1950694a65756acb6e4ca13
Jean Paul Tivan <jptivan53@gmail.com>
2022-12-12 18:39:24 -0400

    Integrando third party custom hooks en Platzi Conf Merch

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,9 +1,34 @@
 import '../styles/components/Information.css';
 
-import { Link } from 'react-router-dom';
-import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useRef, useContext } from 'react';
+import AppContext from '../context/AppContext';
 
 const Information = () => {
+  const form = useRef(null);
+  const navigate = useNavigate();
+  const { state, addToBuyer } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current);
+
+    const buyer = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      address: formData.get('address'),
+      apto: formData.get('apto'),
+      city: formData.get('city'),
+      country: formData.get('country'),
+      state: formData.get('state'),
+      cp: formData.get('cp'),
+      phone: formData.get('phone'),
+    };
+
+    addToBuyer(buyer);
+    navigate('/checkout/payment');
+  };
+
   return (
     <div className="Information">
       <div className="Information-content">
@@ -11,7 +36,7 @@ const Information = () => {
           <h2>Información de contacto</h2>
         </div>
         <div className="Information-form">
-          <form action="">
+          <form ref={form}>
             <input type="text" placeholder="Nombre completo" name="name" id="name" />
             <input type="text" placeholder="Correo electronico" name="email" id="email" />
             <input type="text" placeholder="Dirección" name="address" id="address" />
@@ -24,20 +49,26 @@ const Information = () => {
           </form>
         </div>
         <div className="Information-buttons">
-          <div className="Information-back">Regresar</div>
+          <div className="Information-back">
+            <Link to="/checkout">Regresar</Link>
+          </div>
           <div className="Information-next">
-            <Link to="/checkout/payment">Pagar</Link>
+            <button onClick={handleSubmit}>Pagar</button>
           </div>
         </div>
       </div>
       <div className="Information-sidebar">
         <h3>Pedido</h3>
-        <div className="Information-item">
-          <div className="Information-element">
-            <h4>Item name</h4>
-            <span>$ 132</span>
+        {cart.map(({ product, qty }) => (
+          <div className="Information-item" key={product.id}>
+            <div className="Information-element">
+              <h4>{product.title}</h4>
+              <span>
+                {qty} x ${product.price}
+              </span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
